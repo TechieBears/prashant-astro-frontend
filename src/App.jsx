493ba7183a02bb71a38d110c8125face87ed221d
Shubham Handergule <shubhamhandergule77@gmail.com>
2025-10-31 16:22:25 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -78,29 +78,6 @@ const App = () => {
                             gutter={8}
                             containerClassName=""
                             containerStyle={{}}
-                            toastOptions={{
-                                // Default options for all toasts
-                                duration: 3000,
-                                style: {
-                                    background: '#363636',
-                                    color: '#fff',
-                                },
-                                // Default options for specific types
-                                success: {
-                                    duration: 2000,
-                                    style: {
-                                        background: '#10B981',
-                                        color: '#fff',
-                                    },
-                                },
-                                error: {
-                                    duration: 4000,
-                                    style: {
-                                        background: '#EF4444',
-                                        color: '#fff',
-                                    },
-                                },
-                            }}
                         />
                         <ComingSoonModal isVisible={homeModalStatus} />
                     </PrimeReactProvider>
