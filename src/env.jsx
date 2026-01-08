ddfe9d352ad9d99d3c75b68a3a1eeacd9e4d7d78
aniketmyana <50293654+aniketmyana@users.noreply.github.com>
2026-01-08 13:49:00 +0530

    Update API URLs for production and development

--- src/env.jsx
+++ src/env.jsx
@@ -8,13 +8,13 @@ export const environment = {
     // production: false
 
     // // -------------------- Devlopment Server Api URL --------------------
-    baseUrl: 'https://dev-api.astroguid.com/api/',
-    production: false,
+    // baseUrl: 'https://dev-api.astroguid.com/api/',
+    // production: false,
 
     // -------------------- Live Server Api URL --------------------
 
-    // baseUrl: 'https://api.astroguid.com/api/',
-    // production: true,
+    baseUrl: 'https://api.astroguid.com/api/',
+    production: true,
 
     razorpayKey: 'rzp_test_Rufkt1nYns8XZT'
 }
